40066e593224577237170c2e03b84cc0d713c1df
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-23 20:21:36 +0530

    Added typescript in the backend

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
