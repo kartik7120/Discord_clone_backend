a04aa815e3e68d3a9afdf2c33d02a77b2836044d
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-24 10:45:39 +0530

    Added cors as a dependency

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import { createServer } from "http";
 import dotenv from "dotenv";
+import { Server } from "socket.io"
 dotenv.config();
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 app.get("/", (req, res) => {
     res.send("Hello World");
 })
 
-app.listen(process.env.PORT, () => {
+io.on("connection", (socket) => {
+    console.log("Socket connected to main namespace");
+})
+
+httpServer.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
 })
