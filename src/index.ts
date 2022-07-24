21dce90411773f0ad1851d10200a83c6fde38dab
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-24 11:11:21 +0530

    stabilished a simple socket.io connection

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import express from "express";
 import { createServer } from "http";
 import dotenv from "dotenv";
+import cors from "cors";
 import { Server } from "socket.io"
 dotenv.config();
 const app = express();
+app.use(cors());
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+});
 app.get("/", (req, res) => {
     res.send("Hello World");
 })
 
 io.on("connection", (socket) => {
     console.log("Socket connected to main namespace");
+    console.log(socket.id);
 })
 
 httpServer.listen(process.env.PORT, () => {
