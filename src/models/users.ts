a793c9cfc8d646bc612f6095c099173f9dad18f9
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-08-15 12:08:02 +0530

    Added user schema

---
+++ src/models/users.ts
@@ -0,0 +1,34 @@
+import mongoose, { Types } from "mongoose";
+
+interface userInterface {
+    username: string,
+    user_id: string,
+    picture: string,
+    friends?: Types.ObjectId,
+    user_channels?: Types.ObjectId[]
+}
+
+const userSchema = new mongoose.Schema<userInterface>({
+    username: {
+        type: String,
+        required: [true, "Please enter username of the user"],
+        minlength: 5
+    },
+    user_id: {
+        type: String,
+        required: [true, "Please enter user_id of the user"]
+    },
+    picture: {
+        type: String,
+        required: [true, "Please provide url for profile picture of the user"]
+    },
+    user_channels: {
+        type: [mongoose.Schema.Types.ObjectId],
+    },
+    friends: {
+        type: [mongoose.Schema.Types.ObjectId]
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+export default User;
