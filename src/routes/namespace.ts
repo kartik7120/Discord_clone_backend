763bcb4a490a9294bcd2936135f0f1ad26f60316
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-08-20 16:05:50 +0530

    removed a commented line

--- src/routes/namespace.ts
+++ src/routes/namespace.ts
@@ -42,7 +42,6 @@ router.post("/createNamespace/:namespace", async (req, res, next) => {
         const currUser = await fetchUser(`${userBody.userSub}`);
         const user = await User.findOneAndUpdate({ user_id: userBody.userSub },
             { $push: { user_channels: newChannel._id } }, { new: true, upsert: true });
-        // console.log(currUser);
         res.json(namespace);
     } catch (error) {
         res.status(500).json("Error occured while creating a namespace, Please try again");
