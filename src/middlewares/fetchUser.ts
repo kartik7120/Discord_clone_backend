1fbf1e929ee081e5fb5f424abc6541f5fd377bc5
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-09-12 18:00:32 +0530

    Changed module from es2015 to es2022

--- src/middlewares/fetchUser.ts
+++ src/middlewares/fetchUser.ts
@@ -1,8 +1,24 @@
 import axios from "axios";
+import dotenv from "dotenv"
+dotenv.config();
 
 const fetchUser = async (userSub: string) => {
     try {
-        const URL = `${process.env.AUTH_MANAGEMENT_API_AUDIENCE}users/${userSub}?include_fields=true`;
+        var options = {
+            method: 'POST',
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            data: new URLSearchParams({
+                grant_type: 'client_credentials',
+                client_id: 'TxZ0uUM1YhCG4ZSxSWcKbz9DEXWXAWTz',
+                client_secret: 'gu9dGI-mYYDotIsCmIBP2dgax4z2vQWTPwtXCk-pBHVn8lhOM0xT3cAkZRWK0dDJ',
+                audience: 'https://dev-6mnzakh2.us.auth0.com/api/v2/'
+            })
+        };
+        const response1 = await axios("https://dev-6mnzakh2.us.auth0.com/oauth/token", options);
+        const token = response1.data.access_token;
+        const token_type = response1.data.token_type;
+
+        const URL = `${token_type} ${token}users/${userSub}?include_fields=true`;
         const config = {
             headers: {
                 'Authorization': process.env.AUTH_MANAGEMENT_API_TOKEN!
